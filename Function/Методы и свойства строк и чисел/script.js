88ecc48256a88c51f63b5756b32d0846b5c637c1
Mikle <melnik.m23@gmail.com>
2022-01-27 20:19:11 +0300

    Методы и свойства у строк и чисел

---
+++ Function/Методы и свойства строк и чисел/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//const str = 'test';
+
+// console.log(str.toUpperCase());  Большой регистр
+//console.log(str.toLowerCase()); // Маленький регистр
+//console.log(str);
+
+//const fruit = "Likes Fruit"; //возвращает часть контента которую укажем, если нет такого контента вернется -1
+// console.log(fruit.indexOf('Fruit'));
+//console.log(fruit.indexOf('q'));
+
+//const logg = 'Hello world';
+//console.log(logg.slice(6, 11)); // Вырезает часть контента которую укажем
+
+//console.log(logg.substring(6, 11)); // Как и прошлый только без отриц. знач.
+//console.log(logg.substr(6, 4)); // Вырезает и отдает знач. 1арг с  какого числа 2й кол-во чисел
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+//console.log(parseInt(test));
+console.log(parseFloat(test));
