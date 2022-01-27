21dc1f369fa4c1ff7d77ab26cf09216d0b88bdce
Mikle <melnik.m23@gmail.com>
2022-01-27 15:43:18 +0300

    Modifications practic

--- Practic/js/script.js
+++ Practic/js/script.js
@@ -7,12 +7,25 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
-const a = prompt('Какой фильм вы смотрели в последний раз ?', ''),
-    b = prompt(' Как вы его оцените ?', ''),
-    c = prompt('Какой фильм вы смотрели в последний раз ?', ''),
-    d = prompt(' Как вы его оцените ?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой фильм вы смотрели в последний раз ?', '');
+    const b = +prompt(' Как вы его оцените ?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 console.log(personalMovieDB);
