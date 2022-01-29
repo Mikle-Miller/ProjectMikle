07de6c7b95e6ee28f7c9d13c14c0c0c638016dbb
Mikle <melnik.m23@gmail.com>
2022-01-29 16:30:05 +0300

    Объекты, диструктуризация объектов

---
+++ Объекты/script.js
@@ -0,0 +1,67 @@
+"use strict";
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function(){
+        console.log('text');
+    }
+
+};
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+//console.log(Object.keys(options).length);
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+let counter = 0;
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);
+
+
+const testObj = {
+    name: 'Mikle',
+    surName: 'Miller',
+    age: 32,
+    status: true,
+    agressiv: undefined,
+    hobby: {
+        passiv: 'games',
+        activ: 'strength_zal',
+    },
+    livesAuto: [
+        'Ford',
+        'Mazda',
+        'Shevrole'
+    ]
+};
+//console.log(Object.keys(testObj).length);
+
+for (let key in testObj) {
+    if (typeof (testObj[key]) === 'object') {
+        for (let i in testObj[key]) {
+            console.log(`${i} : ${testObj[key][i]}`);
+        }
+    } else {
+        console.log(`${key} : ${testObj[key]}`);
+    }
+
+}
